1068a82590d9f185eaf345b5ff334f932f196ea6
shaden240 <shaden2mangour@gmail.com>
2022-12-15 01:11:38 +0300

    complete all the questions

--- Week4/final_project_template.py
+++ Week4/final_project_template.py
@@ -13,25 +13,35 @@ print(movies_dataset.shape)
 #Yes, each row for one movie
 
 #Question 3
-
+#ما هي أطول مدة فيلم موجودة في البيانات؟
 max_value=max(movies_dataset.duration)
 print(max_value)
 
 
 #Question 4
-
-#result_movie_titles=movies_dataset["movie_title"] #هنا طلعت عناوين جميع الافلام
+#ما هو اسم أقصر فيلم موجود في البيانات؟
+#result_movie_titles=movies_dataset["movie_title"] 
 min_duration=min(movies_dataset.duration)
 print(min_duration)#طلعت اقل مدة زمنية رقما
+
 #looping through spesific DataFrames rows   
 for row_label,row in movies_dataset.iterrows():
     if row["duration"]==min_duration: 
         print(row["movie_title"])
+
+
 #Question 5
-#Yes
+#الكود التالي يقوم باضافة عمود جديد إلى البيانات اسمه 
+# "duration_in_minutes"
+#يحتوي على مدة الفيلم الواحد بالساعات
 
 duration_in_minutes=movies_dataset["duration"]/60
 movies_dataset["duration_in_minutes"]=duration_in_minutes
+
+#Yes
+
+#ما هو عدد الأفلام التي قام بإخراجها المخرج
+#James Cameron
 
 #Question 6
 x=movies_dataset['director_name']=="James Cameron"
@@ -39,13 +49,18 @@ df1=movies_dataset[x]
 result=df1["director_name"].count()
 print(result)
 
+#ما هو المتوسط الخاص بمدة الأفلام الموجودة في البيانات
 #Question 7
 print(np.mean(movies_dataset["duration"]))
 
+
+#ما هو الوسيط الخاص بمدة الأفلام الموجودة في البيانات
 #Question 8
 print(np.median(movies_dataset["duration"]))
 
 
+#ما هو عدد الأفلام التي لغتها هي
+#"Danish" أو "Dutch" 
 #Question 9
 y=np.logical_or(movies_dataset['language']=="Danish",
                 movies_dataset['language']=="Dutch")
@@ -54,12 +69,15 @@ df2=movies_dataset[y]
 result2=df2["language"].count()
 print(result2)
 
+#ما هو عدد الأفلام التي تم انتاجها في "USA"
 #Question 10
 z=movies_dataset['country']=="USA"
 df3=movies_dataset[z]
 result3=df3["country"].count()
 print(result3)
 
+#قم بانشاء تقرير يظهر عدد الأفلام الملونة وعدد الأفلام بالأبيض والأسود التي تم انتاجها في "USA"
+#بالاعتماد على هذا  التقرير ما هو عدد الأفلام الملونة وعدد الأفلام بالأبيض والأسود؟
 #Question 11
 
 movies_report_1=pd.pivot_table(df3,index=['is_colored']
@@ -82,7 +100,12 @@ movies_report_2=pd.pivot_table(df3,index=['main_genre']
 print(movies_report_2.sort_values("imdb_score"))
 
 
+
 #Question 13
+#"main genre" قم بانشاء تقرير يظهر عدد الأفلام ومتوسط تقييمات الأفلام حسب ال 
+# التي تم انتاجها في "USA"
+#بالاعتماد على هذا التقرير
+#ما هي أعلى ثلاثة genres حسب متوسط التقييمات؟
 
 movies_report_3=pd.pivot_table(df3,index=['main_genre']
                                   ,values=["imdb_score",
@@ -95,7 +118,9 @@ movies_report_3=pd.pivot_table(df3,index=['main_genre']
 print(movies_report_3.sort_values("budget"))
 
 #Question 14
-
+#قم بانشاء تقرير يظهر متوسط تقييمات الأفلام ومتوسط تكلفة الأفلام  حسب ال "main genre" 
+#التي تم انتاجها في "USA"
+#بالاعتماد على هذا التقرير ما هي أعلى ثلاثة genres حسب متوسط تكلفة الفيلم؟
 movies_report_3=pd.pivot_table(df3,index=['main_genre']
                                   ,values="gross"
                                           
